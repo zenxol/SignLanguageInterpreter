df6d83ded75a07ecb06c2b691cf380f484eb6301
Andy Yu <andyu666@gmail.com>
2024-11-15 19:02:53 -0500

    Create main.py
    Basic cv2 test

---
+++ main.py
@@ -0,0 +1,14 @@
+import cv2 
+
+cap = cv2.VideoCapture(0)
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    cv2.imshow('Live Video', frame)
+    
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    
+cap.release()
+cv2.destroyAllWindows()
+cv2.waitKey(1)
